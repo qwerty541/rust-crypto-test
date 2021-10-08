593ad95a80863809a4ebbb8d4a5efe961da1a3dd
Alexandr <qwerty541zxc@gmail.com>
2021-10-08 15:03:15 +0300

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(non_fmt_panics)]
 
+use core::panic;
 use crypto::aessafe;
 use crypto::blockmodes;
 use crypto::buffer;
@@ -11,6 +12,7 @@ use rand::RngCore;
 use std::cmp::{self, Ord};
 use std::convert::From;
 use std::io;
+use std::iter::Iterator;
 
 pub type AesCbcEncryptor = blockmodes::CbcEncryptor<
     aessafe::AesSafe256Encryptor,
@@ -23,14 +25,13 @@ pub type AesCbcDecryptor = blockmodes::CbcDecryptor<
 >;
 
 const EOF: bool = true;
-const EXPECTED_AES_BLOCK_LENGTH: usize = 16;
 const MINIMUN_ENCRYPTED_PACKAGE_LENGTH: usize = 64;
 const ENCRYPTED_PACKAGE_META_LENGTH: usize = 2;
 
-pub fn decrypt_buf<'a>(
-    decryptor: &mut AesCbcDecryptor,
+pub fn decrypt_buf<'a, 'b>(
+    decryptor: &'a mut AesCbcDecryptor,
     decryptor_block_length: usize,
-    buf: &'a mut [u8],
+    buf: &'b mut [u8],
 ) -> io::Result<Vec<u8>> {
     if buf.len() < MINIMUN_ENCRYPTED_PACKAGE_LENGTH {
         return io::Result::Err(io::Error::new(
@@ -53,8 +54,6 @@ pub fn decrypt_buf<'a>(
             ),
         ));
     }
-
-    dbg!(&buf);
 
     let mut decrypted_buf: Vec<u8> = Vec::new();
     let mut read_buffer = buffer::RefReadBuffer::new(buf);
@@ -91,8 +90,6 @@ pub fn decrypt_buf<'a>(
         }
     }
 
-    dbg!(&decrypted_buf);
-
     let garbage_length = match decrypted_buf.split_last() {
         Some((last, _)) => *last,
         None => {
@@ -103,8 +100,8 @@ pub fn decrypt_buf<'a>(
         }
     };
     let payload_length = decrypted_buf.len() - garbage_length as usize;
-    let payload = decrypted_buf.split_off(payload_length);
-    let garbage_bytes = decrypted_buf;
+    let garbage_bytes = decrypted_buf.split_off(payload_length);
+    let payload = decrypted_buf;
     let input_lrc = match garbage_bytes.first() {
         Some(input_lrc) => *input_lrc,
         None => {
@@ -129,10 +126,10 @@ pub fn decrypt_buf<'a>(
     Ok(payload)
 }
 
-pub fn encrypt_buf<'a>(
-    encryptor: &mut AesCbcEncryptor,
+pub fn encrypt_buf<'a, 'b>(
+    encryptor: &'a mut AesCbcEncryptor,
     encryptor_block_length: usize,
-    buf: &'a [u8],
+    buf: &'b [u8],
 ) -> io::Result<Vec<u8>> {
     if buf.is_empty() {
         return io::Result::Err(io::Error::new(
@@ -156,8 +153,8 @@ pub fn encrypt_buf<'a>(
         }
         cmp::Ordering::Greater => {
             let mut rng = thread_rng();
-            let random_bytes_legth =
-                (buf.len() + ENCRYPTED_PACKAGE_META_LENGTH) % EXPECTED_AES_BLOCK_LENGTH;
+            let random_bytes_legth = encryptor_block_length
+                - ((buf.len() + ENCRYPTED_PACKAGE_META_LENGTH) % encryptor_block_length);
 
             bytes_that_will_be_encrypted.push(lrc);
 
@@ -188,8 +185,9 @@ pub fn encrypt_buf<'a>(
         return io::Result::Err(io::Error::new(
             io::ErrorKind::InvalidData,
             format!(
-                "Length of buffer with bytes that will be encrypted less than {}",
-                MINIMUN_ENCRYPTED_PACKAGE_LENGTH
+                "Length of buffer with bytes that will be encrypted less than {}, its length - {}",
+                MINIMUN_ENCRYPTED_PACKAGE_LENGTH,
+                bytes_that_will_be_encrypted.len()
             ),
         ));
     }
@@ -198,13 +196,12 @@ pub fn encrypt_buf<'a>(
         return io::Result::Err(io::Error::new(
             io::ErrorKind::InvalidData,
             format!(
-                "Length of buffer with bytes that will be enrypted is not divisible by {} without remainder",
-                encryptor_block_length
+                "Length of buffer with bytes that will be enrypted is not divisible by {} without remainder, its length - {}",
+                encryptor_block_length,
+                bytes_that_will_be_encrypted.len()
             ),
         ));
     }
-
-    dbg!(&bytes_that_will_be_encrypted);
 
     let mut encrypted_buf: Vec<u8> = Vec::new();
     let mut read_buffer = buffer::RefReadBuffer::new(bytes_that_will_be_encrypted.as_slice());
@@ -241,23 +238,27 @@ pub fn encrypt_buf<'a>(
         }
     }
 
-    dbg!(&encrypted_buf);
+    if encrypted_buf.len() < MINIMUN_ENCRYPTED_PACKAGE_LENGTH {
+        return io::Result::Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            format!(
+                "Length of encrypted buffer less than {}, its length - {}",
+                MINIMUN_ENCRYPTED_PACKAGE_LENGTH,
+                encrypted_buf.len()
+            ),
+        ));
+    }
 
-    assert!(
-        encrypted_buf.len() >= MINIMUN_ENCRYPTED_PACKAGE_LENGTH,
-        format!(
-            "Encryption algorithm failed: encrypted buffer length less than {}",
-            MINIMUN_ENCRYPTED_PACKAGE_LENGTH
-        )
-    );
-
-    assert!(
-        encrypted_buf.len() % encryptor_block_length == 0_usize,
-        format!(
-            "Encryption algorithm failed: encrypted buffer is not divisible by {} without remained",
-            encryptor_block_length
-        )
-    );
+    if encrypted_buf.len() % encryptor_block_length != 0_usize {
+        return io::Result::Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            format!(
+                "Length of encrypted buffer is not divisible by {} without remainder, its length - {}",
+                encryptor_block_length,
+                encrypted_buf.len()
+            ),
+        ));
+    }
 
     Ok(encrypted_buf)
 }
@@ -278,12 +279,10 @@ fn main() {
         0x0F, 0x0E, 0x0E, 0x0B, 0x08, 0x0B, 0x00, 0x0A,
         0x0F, 0x02, 0x02, 0x01, 0x03, 0x0B, 0x06, 0x03
     ];
-    let mut encryption_iv = [0_u8; 16];
-    let mut decryption_iv = [0_u8; 16];
+    let mut iv = [0_u8; 16];
     let mut os_rng = OsRng::default();
 
-    os_rng.fill_bytes(&mut encryption_iv);
-    os_rng.fill_bytes(&mut decryption_iv);
+    os_rng.fill_bytes(&mut iv);
 
     let aes_encryptor = aessafe::AesSafe256Encryptor::new(shared_secret_key.as_slice());
     let encryptor_block_length =
@@ -297,27 +296,85 @@ fn main() {
             &aes_decryptor,
         );
 
-    let mut encryptor =
-        blockmodes::CbcEncryptor::new(aes_encryptor, blockmodes::NoPadding, encryption_iv.to_vec());
-    let mut decryptor =
-        blockmodes::CbcDecryptor::new(aes_decryptor, blockmodes::NoPadding, decryption_iv.to_vec());
-
     #[rustfmt::skip]
-    let data = vec![
-        0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-        0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10,
-        0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18,
-        0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20
+    let manually_written_datasets: Vec<Vec<u8>> = vec![
+        vec![
+            0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+            // 8 bytes
+        ],
+        vec![
+            0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+            0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10,
+            // 16 bytes
+        ],
+        vec![
+            0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+            0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10,
+            0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18,
+            0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20
+            // 32 bytes
+        ],
+        vec![
+            0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+            0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10,
+            0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18,
+            0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20,
+            0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28,
+            0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F, 0x30,
+            0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38,
+            0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x40
+            // 64 bytes
+        ]
     ];
 
-    let mut encrypted_data =
-        encrypt_buf(&mut encryptor, encryptor_block_length, data.as_slice()).unwrap();
-    let decrypted_data = decrypt_buf(
-        &mut decryptor,
-        decryptor_block_length,
-        encrypted_data.as_mut_slice(),
-    )
-    .unwrap();
+    let mut random_dataset_64 = [0; 8];
+    let mut random_dataset_128 = [0; 16];
+    let mut random_dataset_256 = [0; 32];
+    let mut random_dataset_512 = [0; 64];
+    let mut random_dataset_1024 = [0; 128];
 
-    assert_eq!(data.as_slice(), decrypted_data.as_slice());
+    os_rng.fill_bytes(&mut random_dataset_64);
+    os_rng.fill_bytes(&mut random_dataset_128);
+    os_rng.fill_bytes(&mut random_dataset_256);
+    os_rng.fill_bytes(&mut random_dataset_512);
+    os_rng.fill_bytes(&mut random_dataset_1024);
+
+    let random_datasets: Vec<Vec<u8>> = vec![
+        Vec::from(random_dataset_64),
+        Vec::from(random_dataset_128),
+        Vec::from(random_dataset_256),
+        Vec::from(random_dataset_512),
+        Vec::from(random_dataset_1024),
+    ];
+
+    let mut datasets: Vec<Vec<u8>> = Vec::new();
+    datasets.extend_from_slice(manually_written_datasets.as_slice());
+    datasets.extend_from_slice(random_datasets.as_slice());
+
+    for (i, data) in datasets.iter().enumerate() {
+        let mut encryptor =
+            blockmodes::CbcEncryptor::new(aes_encryptor, blockmodes::NoPadding, iv.to_vec());
+        let mut decryptor =
+            blockmodes::CbcDecryptor::new(aes_decryptor, blockmodes::NoPadding, iv.to_vec());
+
+        let mut encrypted_data =
+            encrypt_buf(&mut encryptor, encryptor_block_length, data.as_slice()).unwrap_or_else(
+                |error| panic!("failed to encrypt dataset #{} because of: {}", i + 1, error),
+            );
+        let decrypted_data = decrypt_buf(
+            &mut decryptor,
+            decryptor_block_length,
+            encrypted_data.as_mut_slice(),
+        )
+        .unwrap_or_else(|error| {
+            panic!("failed to decrypt dataset #{} because of: {}", i + 1, error)
+        });
+
+        assert_eq!(data.as_slice(), decrypted_data.as_slice());
+        println!(
+            "Dataset #{} with {} bytes successfully passed test",
+            i + 1,
+            data.len()
+        );
+    }
 }
